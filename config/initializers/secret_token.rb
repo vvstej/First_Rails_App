bd34ea19372e6f032ae207e9065da0cb14478d0b
Venkata <vvepa@salesforce.com>
2014-07-05 01:53:50 -0700

    First App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '3a93c7b8135d90fb588080bef28bace3a421d98810719448a744a46d10e45803cf05f102d0f85c969e04a6a3c8c036622b3fdb695ebf8571f8427e4c55886c98'
